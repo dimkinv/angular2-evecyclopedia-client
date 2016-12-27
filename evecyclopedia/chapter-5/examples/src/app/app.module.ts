eb0f3711a43753c9b2e8667dd4a23a91e5c96707
Danny Vernovsky <dv696w@att.com>
2016-12-27 12:23:33 +0200

    reversed examples chapter 5 mistake

--- evecyclopedia/chapter-5/examples/src/app/app.module.ts
+++ evecyclopedia/chapter-5/examples/src/app/app.module.ts
@@ -2,9 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import {RouterModule} from '@angular/router';
 
-import {routes} from './app-routes';
 import {AppComponent} from './app.component';
 import {MainViewComponent} from './ui/main-view/main-view.component';
 import {DetailsViewComponent} from './ui/details-view/details-view.component';
@@ -16,7 +14,6 @@ import {DetailsViewComponent} from './ui/details-view/details-view.component';
     DetailsViewComponent
   ],
   imports: [
-    RouterModule.forRoot(routes),
     BrowserModule,
     FormsModule,
     HttpModule
