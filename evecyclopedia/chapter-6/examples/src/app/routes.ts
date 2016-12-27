2a6939936d768582d42912587c5bcb33f4c91e6a
Danny Vernovsky <dv696w@att.com>
2016-12-27 17:03:43 +0200

    added guards

--- evecyclopedia/chapter-6/examples/src/app/routes.ts
+++ evecyclopedia/chapter-6/examples/src/app/routes.ts
@@ -1,16 +1,29 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './ui/about/about.component';
 import { HomeComponent } from './ui/home/home.component';
+import { SubHomeComponent } from './ui/home/sub-home/sub-home.component';
 import { NewsComponent } from './ui/news/news.component';
 import { HeaderComponent } from './ui/header/header.component';
+import { AuthGuard } from './auth/auth-guard/auth.guard';
 
 export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+        children: [
+            {
+                path: 'sub',
+                component: SubHomeComponent
+            }
+        ]
+    },
     {
         path: 'home',
         component: HomeComponent,
     },
     {
         path: 'news',
+        // canActivate: [AuthGuard],
         component: NewsComponent,
     },
     {
@@ -26,5 +39,5 @@ export const routes: Routes = [
     //     component: HeaderComponent,
     //     outlet: 'header'
     // },
-    {path: '**', redirectTo: 'home' }
+    { path: '**', redirectTo: 'home' }
 ]
---
+++ evecyclopedia/chapter-6/examples/src/app/ui/home/sub-home/sub-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'sub-home',
+    templateUrl: './sub-home.html',
+})
+export class SubHomeComponent implements OnInit {
+    private value: string;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
