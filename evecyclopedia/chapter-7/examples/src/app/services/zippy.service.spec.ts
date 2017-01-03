8edde8a353b4a3d5b20d0dd97737d73a671bcad0
Nabil Naffer <naffar.nabil@gmail.com>
2017-01-03 14:54:20 +0200

    Service unit tests

--- evecyclopedia/chapter-7/examples/src/app/services/zippy.service.spec.ts
+++ evecyclopedia/chapter-7/examples/src/app/services/zippy.service.spec.ts
@@ -11,10 +11,15 @@ describe('Zippy Service isolated unit tests', ()=>{
 });
 
 //Using angular tools to provide the service
-// describe('Zippy Service isolated unit tests', ()=>{
-//     let zippyService:ZippyService = new ZippyService();
-//     it('should return text when calling getText method', ()=> {
-//         let text = zippyService.getText();
-//         expect(text.length).toBeGreaterThan(0);
-//     });
-// });
+import { TestBed, inject, async } from '@angular/core/testing';
+
+describe('Zippy Service isolated unit tests', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({ providers: [ZippyService] });
+    });
+    it('test should wait for FancyService.getTimeoutValue',
+        async(inject([ZippyService], (zippyService: ZippyService) => {
+            let text = zippyService.getText();
+            expect(text.length).toBeGreaterThan(0);
+        })));
+});
