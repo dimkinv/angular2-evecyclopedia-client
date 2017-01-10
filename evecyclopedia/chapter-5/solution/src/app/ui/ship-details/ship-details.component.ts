68bd9c2db982e8d62a6bab3c8d12c30ff9555f7b
Nabil Naffer <naffar.nabil@gmail.com>
2017-01-10 14:32:04 +0200

    chap5 Solution fix

--- evecyclopedia/chapter-5/solution/src/app/ui/ship-details/ship-details.component.ts
+++ evecyclopedia/chapter-5/solution/src/app/ui/ship-details/ship-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges} from '@angular/core';
+import {Component, Input, ChangeDetectionStrategy} from '@angular/core';
 import {ShipSelector} from '../../models/ship-selector.model';
 import {Ship} from '../../models/ship.model';
 import {ShipsService} from '../../services/ships.service';
@@ -6,24 +6,14 @@ import {ShipsService} from '../../services/ships.service';
 @Component({
   selector: 'ship-details',
   styleUrls: ['ship-details.component.less'],
-  templateUrl: 'ship-details.component.html'
+  templateUrl: 'ship-details.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ShipDetailsComponent implements OnChanges {
-  @Input()
-  shipSelector: ShipSelector;
-
+export class ShipDetailsComponent {
+  @Input()  
   ship:Ship;
 
-  constructor(private shipsService:ShipsService) {}
-
-  ngOnChanges(changeObj){
-    if(changeObj.shipSelector.currentValue){
-      this.shipsService.getShip(this.shipSelector.groupName, this.shipSelector.raceName, this.shipSelector.shipName)
-                        .subscribe(shipDetails => {
-                          this.ship = shipDetails
-                        });
-    }
-  }
+  constructor() {}
 
   get encodedShipName(){
     return encodeURI(this.ship.name)
