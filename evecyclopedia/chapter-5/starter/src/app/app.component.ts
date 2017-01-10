7522aa75f4d7832cf90ec0e944cf3b7eae6f99af
Danny Vernovsky <dv696w@att.com>
2017-01-10 14:50:39 +0200

    fixed chapter 5 starter

--- evecyclopedia/chapter-5/starter/src/app/app.component.ts
+++ evecyclopedia/chapter-5/starter/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-import {ShipSelector} from './models/ship-selector.model';
+import { ShipSelector } from './models/ship-selector.model';
+import { ShipsService } from './services/ships.service';
+import { Ship } from './models/ship.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  shipSelector:ShipSelector;
+  private ship: Ship;
+  constructor(private shipsService: ShipsService) { }
+
+  onShipSelected(shipSelector: ShipSelector) {
+    this.ship = this.shipsService.getShip(shipSelector.groupName, shipSelector.raceName, shipSelector.shipName);
+  }
 }
